78aa054b7190e1f015b9e358969d5901fd6caa19
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-11-04 13:15:48 +0100

    adding some fun

--- setup.py
+++ setup.py
@@ -12,16 +12,13 @@ setup(
     author='J. Michael Burgess',
     author_email='jmichaelburgess@gmail.com',
     #   url = 'https://github.com/grburgess/pychangcooper',
- #   download_url='https://github.com/grburgess/pychangcooper/archive/1.1.2.tar.gz',
+    #   download_url='https://github.com/grburgess/pychangcooper/archive/1.1.2.tar.gz',
 
-#    package_data={'': extra_files, },
-#    include_package_data=True,
-
-#    ext_modules=cythonize('popsynth/broken_powerlaw_sampler.pyx'),
     install_requires=[
         'numpy',
         'scipy',
         'h5py',
-        'pystan'
+        'pystan',
+        'pandas'
     ],
 )
---
+++ stan_utility/__init__.py
@@ -0,0 +1,14 @@
+from save_fit import stanfit_to_hdf5, StanSavedFit
+import stan_utility
+from stan_utility import compile_model, check_all_diagnostics
+
+
+
+__all__ = ['stanfit_to_hdf5',
+           'StanSavedFit',
+           'stan_utility',
+           'compile_model',
+           'check_all_diagnostics'
+
+
+]
