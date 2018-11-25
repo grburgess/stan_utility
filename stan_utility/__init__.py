a923ea1fc4e0a2372410734ac5090d31ed84207a
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-11-25 14:41:48 +0100

    add gen

--- stan_utility/__init__.py
+++ stan_utility/__init__.py
@@ -1,6 +1,6 @@
 from stan_utility.utils import compile_model, check_all_diagnostics
 from stan_utility.save_fit import stanfit_to_hdf5, StanSavedFit
-
+from stan_utility.stan_generator import StanGenerator
 
 __all__ = ['stanfit_to_hdf5',
            'StanSavedFit',
